6326f8d69e7b8c48bba4108ecd5eae29d47cf896
LeonGB <leon.gb90@gmail.com>
2017-09-09 12:36:20 +0200

    Fixed message hashing and struct before keygen

--- src/tests/leon_test.cpp
+++ src/tests/leon_test.cpp
@@ -16,9 +16,23 @@ namespace {
         bds_state s;
         unsigned char seed[32];
         unsigned char h = 8;
+            unsigned int k = 2;
         unsigned int n = 32;
         printf("before keygen");
-        bds_state *state = &s;
+
+            unsigned char stack[(h+1)*n];
+            unsigned int stackoffset = 0;
+            unsigned char stacklevels[h+1];
+            unsigned char auth[(h)*n];
+            unsigned char keep[(h >> 1)*n];
+            treehash_inst treehash[h-k];
+            unsigned char th_nodes[(h-k)*n];
+            unsigned char retain[((1 << k) - k - 1)*n];
+            bds_state *state = &s;
+            for (int i = 0; i < h-k; i++)
+                    treehash[i].node = &th_nodes[n*i];
+            xmss_set_bds_state(state, stack, stackoffset, stacklevels, auth, keep, treehash, retain, 0);
+
         xmssfast_Genkeypair(pk, sk, state,seed, h);
 
         for (int i = 0; i < n; i++) {
@@ -29,7 +43,7 @@ namespace {
 
         unsigned char msg[32] = {0};
         unsigned long long siglen = 4 + 32 + 67 * 32 + h * 32 + 32;
-        unsigned char sign[2468] = {0};
+        unsigned char sign[10000] = {0};
         siglen = 0;
         int x;
         x = xmssfast_Signmsg(sk, state, sign, msg,32, h);
