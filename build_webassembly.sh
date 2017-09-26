7d7c8d889ad418a44c68c2c2b088006622711e4e
Scott Donald <scottdonald@protonmail.ch>
2017-09-26 21:02:51 +1000

    Build web assembly JS file separately

--- build_webassembly.sh
+++ build_webassembly.sh
@@ -7,6 +7,7 @@ echo "Building..."
 emmake make
 echo "Emscripten Binding/Optimizing..."
 emcc --bind libjsqrl.so -O3 -o libjsqrl.js
+emcc --bind libjsqrl.so -O3 -s WASM=1 -o web-libjsqrl.js
 echo "Running test"
 cp ../src/tests_js/test.js .
 node test.js
