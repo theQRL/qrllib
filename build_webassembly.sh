71e7db0999824565e5343d821b41d994c0979c96
JP Lomas <jp.lomas@gmail.com>
2017-09-30 21:44:39 +0100

    Initial npm deploy

--- build_webassembly.sh
+++ build_webassembly.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-mkdir -p ./build
-cd ./build
+mkdir -p ./build/
+mkdir -p ./build/js
+cd ./build/js
 echo "Running CMAKE..."
-emconfigure cmake -DBUILD_WEBASSEMBLY=ON -DBUILD_PYTHON=OFF -DCMAKE_BUILD_TYPE=Release ..
+emconfigure cmake -DBUILD_WEBASSEMBLY=ON -DBUILD_PYTHON=OFF -DCMAKE_BUILD_TYPE=Release ../..
 echo "Building..."
 emmake make
 echo "Emscripten Binding/Optimizing..."
 emcc --bind libjsqrl.so -O3 -o libjsqrl.js
 emcc --bind libjsqrl.so -O3 -s WASM=1 -o web-libjsqrl.js
 echo "Running test"
-cp ../src/tests_js/test.js .
+cp ../../src/tests_js/test.js .
 node test.js
