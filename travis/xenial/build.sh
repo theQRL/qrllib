b65980a8d688d8e8dd31514f5b833ebd6351b10b
Juan Leni <lenijuan@gmail.com>
2017-10-02 06:44:13 +0200

    Improving travis config

--- travis/xenial/build.sh
+++ travis/xenial/build.sh
@@ -1,13 +1,22 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-BUILD_DIR="cmake-build-${PLATFORM}"
+BUILD_DIR="build"
+
+set -e
 
 cd /travis
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
-cmake -DCMAKE_C_COMPILER=gcc-${CC_VER} -DCMAKE_CXX_COMPILER=g++-${CC_VER} /travis
+cmake -DCMAKE_C_COMPILER=gcc-${CC_VER} -DCMAKE_CXX_COMPILER=g++-${CC_VER} ${CMAKE_ARGS} /travis
 make
 
-cd /travis
-python3 setup.py sdist
+if [ -n "${TEST:+1}" ]; then
+  echo "Running Tests"
+  export GTEST_COLOR=1
+  ctest -VV
+fi
 
+if [ -n "${DEPLOY:+1}" ]; then
+    cd /travis
+    python3 setup.py sdist
+fi
