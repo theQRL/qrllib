771859782d4a8a6a5c6bbf280986f0f6056c3541
Scott Donald <scottdonald@protonmail.ch>
2017-10-16 21:39:45 +1100

    Adding Meteor variable scoping fix for web-libjsqrl.js

--- build_webassembly.sh
+++ build_webassembly.sh
@@ -10,6 +10,8 @@ emmake make
 echo "Emscripten Binding/Optimizing..."
 emcc --bind libjsqrl.so -O3 -o libjsqrl.js
 emcc --bind libjsqrl.so -O3 -s WASM=1 -o web-libjsqrl.js
+echo "QRLLIB=Module;" >> web-libjsqrl.js
+
 echo "Running test"
 cp ../../src/tests_js/test.js .
 node test.js
