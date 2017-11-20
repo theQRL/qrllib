0f9e574343bcd2d5b2cb256674f63bc87b43435b
Andrew Chiw <randomshinichi4869@gmail.com>
2017-11-20 16:06:59 +0000

    More robust push_debs script, checks if branch exists first. Good for not nuking dev directory
    Had to rewrite merge git branches PLATFORM script
    
    Don't need ls -lh and pwd before the push_debs_github.sh scripts anymore
    
    Code to check if branch already exists wasn't running
    WILL NOT put Git https URL in a variable
    why?
    because if I go GIT_HTTPS_REPO=https://.....$GITHUB_TOKEN..... $GITHUB_TOKEN will not be evaluated.
    Actually, it will be if you execute it, but if you source it, then it won't. Or if you put single quotes around it... I don't know.
    I don't want to deal with this complexity so I'm just not gonna use it.
    
    travis/env env_file will stay - this is for passing relatively unchanging ENVVARS to the Docker container
    
    revised checking for branch

--- travis/merge_debs_ghpages.sh
+++ travis/merge_debs_ghpages.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
-
+ls -lh
 # Travis only clones the master branch, so I have to clone everything again to make sure I can switch branches.
-mkdir working_dir
-git clone https://github.com/randomshinichi/qrllib.git working_dir/qrllib
-cd working_dir/qrllib
+mkdir merging_dir
+git clone https://github.com/randomshinichi/qrllib.git merging_dir/qrllib
+cd merging_dir/qrllib
 git remote -v
 git branch -a
 
 git checkout --track origin/xenial
-cp -r built/* ..
+mkdir ../xenial
+cp -r results/* ../xenial/
 
 git checkout --track origin/stretch
-cp -r built/* ..
+mkdir ../stretch
+cp -r results/* ../stretch/
 
 git checkout --track origin/gh-pages
 rm -rf * .gitignore .travis.yml
-cp -r ../xenial ../stretch .
+mv  ../xenial ../stretch .
 
 git add .
 git commit -m "qrllib release debs"
--- travis/xenial/push_debs_github.sh
+++ travis/xenial/push_debs_github.sh
@@ -1,8 +1,13 @@
+set -e
 echo "PUSHING TO GITHUB PAGES"
-pwd
-ls -lh
+if [ `git rev-parse --quiet --verify ${PLATFORM}` > /dev/null ]
+then
+    echo "Branch ${PLATFORM} already exists, deleting"
+    git branch -D ${PLATFORM}
+fi
 git checkout --orphan ${PLATFORM}
 git rm -rf .
 git add results/
 git commit -m "pyqrllib ${PLATFORM} release"
-git push https://randomshinichi:$GITHUB_TOKEN@github.com/randomshinichi/qrllib.git HEAD:${PLATFORM} -f
+
+git push https://randomshinichi:${GITHUB_TOKEN}@github.com/randomshinichi/qrllib.git HEAD:${PLATFORM} -f
