d4d74f74d9615025a0e253754040d2d4f630dced
Juan Leni <lenijuan@gmail.com>
2018-02-11 22:15:25 +0100

    Splitting to improve testability

--- src/xmss-alt/wots_internal.h
+++ src/xmss-alt/wots_internal.h
@@ -6,6 +6,7 @@
 
 #include <cstdint>
 #include "eHashFunctions.h"
+#include "wots_params.h"
 
 void expand_seed(eHashFunction hash_func,
                  unsigned char *outseeds,
@@ -13,4 +14,12 @@ void expand_seed(eHashFunction hash_func,
                  const uint32_t n,
                  const uint32_t len);
 
+void gen_chain(eHashFunction hash_func,
+               unsigned char *out,
+               const unsigned char *in,
+               unsigned int start,
+               unsigned int steps,
+               const wots_params *params,
+               const unsigned char *pub_seed,
+               uint32_t addr[8]);
 #endif
