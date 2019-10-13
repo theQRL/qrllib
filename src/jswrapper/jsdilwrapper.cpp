794116a3d5122b2bcbed4fbaa9e00ecbded44a41
ademcan <ademcan@ademcan.net>
2019-10-13 12:18:09 +0200

    Implement getPK and getSK dilithium JS wrapper

--- src/jswrapper/jsdilwrapper.cpp
+++ src/jswrapper/jsdilwrapper.cpp
@@ -1,6 +1,73 @@
 #include <emscripten.h>
 #include <emscripten/bind.h>
 #include <iostream>
+#include <dilithium.h>
+#include <misc.h>
 
 namespace {
+    class DilithiumWrapper {
+    explicit DilithiumWrapper()
+        :_dilithium() { }
+    
+    public:
+
+        std::vector<uint8_t> getSK()
+        {
+            return _dilithium.getSK();
+        }
+
+        static DilithiumWrapper empty()
+        {
+            return DilithiumWrapper();
+        }
+
+        std::vector<uint8_t> getPKRaw()
+        {   
+            return _dilithium.getPK() ;
+        }
+
+        std::string getPK()
+        {
+            return bin2hstr( _dilithium.getPK() );
+        }
+
+    private:
+        Dilithium _dilithium;
+};
+
+std::string EMSCRIPTEN_KEEPALIVE
+_bin2hstr(const std::vector<unsigned char>& input)
+{
+    return bin2hstr(input, 0);
+}
+
+std::string EMSCRIPTEN_KEEPALIVE
+_getString()
+{
+    return "Another Test from Dilithium JS Wrapper";
+}
+
+int EMSCRIPTEN_KEEPALIVE
+crypto_sign_keypair(
+    unsigned char pk,
+    unsigned char sk)
+{
+    return crypto_sign_keypair(pk, sk);
+}
+
+using namespace emscripten;
+
+EMSCRIPTEN_BINDINGS(my_module) {
+
+    function("getString", &_getString);
+    function("crypto_sign_keypair", &crypto_sign_keypair);
+    function("bin2hstr", &_bin2hstr);
+
+    class_<DilithiumWrapper>("Dilithium")
+        .class_function("empty", &DilithiumWrapper::empty)
+        .function("getPKRaw", &DilithiumWrapper::getPKRaw)
+        .function("getPK", &DilithiumWrapper::getPK)
+        .function("getSK", &DilithiumWrapper::getSK);
+    }
+
 }
