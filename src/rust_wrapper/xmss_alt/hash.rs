2ff00ca3bb6480af5f4be4e15f2467b880639786
Liam Arzola <liam0215@gmail.com>
2022-03-05 14:28:05 -0500

    hash rust implementation

--- src/rust_wrapper/xmss_alt/hash.rs
+++ src/rust_wrapper/xmss_alt/hash.rs
@@ -14,7 +14,7 @@ pub fn u32_slice_to_bytes(input: &[u32], num_bytes: usize) -> Vec<u8> {
     bytes[0..num_bytes].to_vec()
 }
 
-fn addr_to_byte<'a>(bytes: &'a mut [u8], addr: &[u32; 8]) -> &'a [u8] {
+pub fn addr_to_byte<'a>(bytes: &'a mut [u8], addr: &[u32; 8]) -> &'a [u8] {
     if cfg!(target_endian = "little") {
         let bytes_length = bytes.len();
         for i in 0..8 {
@@ -27,7 +27,7 @@ fn addr_to_byte<'a>(bytes: &'a mut [u8], addr: &[u32; 8]) -> &'a [u8] {
     }
 }
 
-fn core_hash(
+pub fn core_hash(
     hash_func: &HashFunction,
     out: &mut [u8],
     type_t: u32,
@@ -83,6 +83,71 @@ fn core_hash(
  */
 pub fn prf(hash_func: &HashFunction, out: &mut [u8], input: &[u8], key: &[u8], keylen: u32) -> u32 {
     return core_hash(hash_func, out, 3, key, keylen, input, 32, keylen);
+}
+
+/*
+ * Implemts H_msg
+ */
+pub fn h_msg(
+    hash_func: &HashFunction,
+    out: &mut [u8],
+    input: &[u8],
+    inlen: u64,
+    key: &[u8],
+    keylen: u32,
+    n: u32,
+) -> u32 {
+    if keylen != 3 * n {
+        eprintln!(
+            "H_msg takes 3n-bit keys, we got n={} but a keylength of {}.\n",
+            n, keylen
+        );
+        return 1;
+    }
+    return core_hash(
+        hash_func,
+        out,
+        2,
+        key,
+        keylen,
+        input,
+        inlen.try_into().unwrap(),
+        n,
+    );
+}
+
+/**
+ * We assume the left half is in input[0]...input[n-1]
+ */
+pub fn hash_h(
+    hash_func: &HashFunction,
+    out: &mut [u8],
+    input: &[u8],
+    pub_seed: &[u8],
+    addr: &mut [u32; 8],
+    n: u32,
+) -> u32 {
+    let mut buf: Vec<u8> = vec![0; 2 * n as usize];
+    let mut key: Vec<u8> = vec![0; n as usize];
+    let mut bitmask: Vec<u8> = vec![0; 2 * n as usize];
+    let mut byte_addr: Vec<u8> = vec![0; 32];
+
+    set_key_and_mask(addr, 0);
+    addr_to_byte(&mut byte_addr, addr);
+    prf(hash_func, &mut key, &byte_addr, pub_seed, n);
+    // Use MSB order
+    set_key_and_mask(addr, 1);
+    addr_to_byte(&mut byte_addr, addr);
+    prf(hash_func, &mut bitmask, &byte_addr, pub_seed, n);
+    set_key_and_mask(addr, 2);
+    addr_to_byte(&mut byte_addr, addr);
+    let out_length = bitmask.len();
+    let bitmask_out = bitmask.get_mut(n as usize..out_length).unwrap();
+    prf(hash_func, bitmask_out, &byte_addr, pub_seed, n);
+    for i in 0..(2 * n as usize) {
+        buf[i] = input[i] ^ bitmask[i];
+    }
+    return core_hash(hash_func, out, 1, &key, n, &buf, 2 * n, n);
 }
 
 pub fn hash_f(
