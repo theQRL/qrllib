70d6ce713926866427d908da0b4105d71c008f60
JP Lomas <jp@theqrl.org>
2025-10-27 15:17:41 +0000

    Update js tests & node version

--- .circleci/build_emscripten.sh
+++ .circleci/build_emscripten.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-emconfigure cmake -DBUILD_WEBASSEMBLY=ON -DCMAKE_BUILD_TYPE=Release
+emcmake cmake -DBUILD_WEBASSEMBLY=ON -DCMAKE_BUILD_TYPE=Release
 emmake make
 
-# FIXME: Disable .mem for node.js until this gets fixed: https://github.com/kripken/emscripten/issues/2542
-emcc --bind libjsqrl.so -s DISABLE_EXCEPTION_CATCHING=0 -O3 --memory-init-file 0  -o libjsqrl.js
-emcc --bind libjsqrl.so -s DISABLE_EXCEPTION_CATCHING=0 -O3 -s WASM=1 -o web-libjsqrl.js
-emcc --bind libjsqrl.so -s DISABLE_EXCEPTION_CATCHING=0 -O3 -s WASM=1 -s SINGLE_FILE=1 -o offline-libjsqrl.js
-emcc --bind libjsdilithium.so -s DISABLE_EXCEPTION_CATCHING=0 -O3 -s WASM=1 -s SINGLE_FILE=1 -o offline-libjsdilithium.js
-emcc --bind libjskyber.so -s DISABLE_EXCEPTION_CATCHING=0 -O3 -s WASM=1 -s SINGLE_FILE=1 -o offline-libjskyber.js
+# Build with modern emscripten (include wrapper object files)
+emcc --bind CMakeFiles/jsqrl.dir/src/jswrapper/jsqrlwrapper.cpp.o libjsqrl.a libqrllib.a libshasha.a -s DISABLE_EXCEPTION_CATCHING=0 -O3 -o libjsqrl.js
+emcc --bind CMakeFiles/jsqrl.dir/src/jswrapper/jsqrlwrapper.cpp.o libjsqrl.a libqrllib.a libshasha.a -s DISABLE_EXCEPTION_CATCHING=0 -O3 -s WASM=1 -o web-libjsqrl.js
+emcc --bind CMakeFiles/jsqrl.dir/src/jswrapper/jsqrlwrapper.cpp.o libjsqrl.a libqrllib.a libshasha.a -s DISABLE_EXCEPTION_CATCHING=0 -O3 -s WASM=1 -s SINGLE_FILE=1 -o offline-libjsqrl.js
+emcc --bind CMakeFiles/jsdilithium.dir/src/jswrapper/jsdilwrapper.cpp.o libjsdilithium.a libdilithium.a libshasha.a -s DISABLE_EXCEPTION_CATCHING=0 -O3 -s WASM=1 -s SINGLE_FILE=1 -o offline-libjsdilithium.js
+emcc --bind CMakeFiles/jskyber.dir/src/jswrapper/jskybwrapper.cpp.o libjskyber.a libkyber.a libshasha.a -s DISABLE_EXCEPTION_CATCHING=0 -O3 -s WASM=1 -s SINGLE_FILE=1 -o offline-libjskyber.js
 echo "QRLLIB=Module;" >> web-libjsqrl.js
 echo "QRLLIB=Module;" >> offline-libjsqrl.js
 echo "DILLIB=Module;" >> offline-libjsdilithium.js
 echo "KYBLIB=Module;" >> offline-libjskyber.js
 
-# Fix paths of web-libjsqrl.wasm for web clients
-sed -i 's/web-libjsqrl\.wasm/\/web-libjsqrl\.wasm/g' web-libjsqrl.js
+# Fix paths of web-libjsqrl.wasm for web clients (macOS compatible)
+sed -i.bak 's/web-libjsqrl\.wasm/\/web-libjsqrl\.wasm/g' web-libjsqrl.js && rm -f web-libjsqrl.js.bak
 
 # Copy to local dir in case it is run locally, the output is shared
 if test -d /tmp/_circleci_local_build_repo; then cp *.js /tmp/_circleci_local_build_repo/tests/js/tmp/; fi
